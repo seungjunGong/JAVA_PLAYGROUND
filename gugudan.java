8cd563c84d8f3f295c5fe6a65a1c18ce5829be57
seungjunGong <jjw5741@naver.com>
2022-07-27 16:03:11 +0900

    for문과 배열을 이용해 구구단을 전부 출력

--- gugudan.java
+++ gugudan.java
@@ -9,13 +9,25 @@ public class gugudan {
         int number = scanner.nextInt(); // Read uesr input
         System.out.println("number : " + number);
 
+        // 2에서 9까지 출력
+
+        int[] sum = new int[9];
+        for (int k = 2; k <= 9; k++) {
+            for (int i = 0; i < sum.length; i++){
+                sum[i] = k * (i+1);
+                System.out.print(sum[i] + " ");
+            }
+            System.out.println("");
+        }
+
+
         // 2와 9사이의 숫자 입력 아닐 시 오류
         if (number < 2) {
             System.out.println("Error입니다. 2와 9사이의 숫자를 입력해주세요.");
         } else if (number > 9) {
             System.out.println("Error입니다. 2와 9사이의 숫자를 입력해주세요.");
         } else {
-            System.out.println("for문으로 출력");
+            System.out.println("배열 값을 받아 반복문 출력");
             for (int i = 1; i < 10; i++) {
                 int result = number * i;
                 System.out.println(result);
