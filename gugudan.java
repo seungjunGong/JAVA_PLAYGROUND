d0ae3cbedc670e3a62bfb8b47c61c94efe193649
seungjunGong <jjw5741@naver.com>
2022-07-27 14:18:21 +0900

    구구단 입력 시 출력ver1

--- gugudan.java
+++ gugudan.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class gugudan {
     public static void main(String[] args){
        System.out.println("구구단 중 출력할 단은? : ");
-       Scanner scanner = new Scanner(System.in);
-       int number = scanner.nextInt();
+       Scanner scanner = new Scanner(System.in); // Create Scanner
+       int number = scanner.nextInt(); // Read uesr input
        System.out.println("number : " + number);
 
     
@@ -27,23 +27,9 @@ public class gugudan {
        result = number * 9;
        System.out.println(result);
 
-       System.out.println("5단");
-       result = number * 1;
-       System.out.println(result);
-       result = number * 2;
-       System.out.println(result);
-       result = number * 3;
-       System.out.println(result);
-       result = number * 4;
-       System.out.println(result);
-       result = number * 5;
-       System.out.println(result);
-       result = number * 6;
-       System.out.println(result);
-       result = number * 8;
-       System.out.println(result);
-       result = number * 9;
-       System.out.println(result);
+    
+
+       scanner.close();
     }
     
 }
