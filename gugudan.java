5c3313d20e6313797cf5737dcbe6715720cfb2b0
seungjunGong <jjw5741@naver.com>
2022-07-27 13:45:55 +0900

    2단 3단 수정

--- gugudan.java
+++ gugudan.java
@@ -1,43 +1,27 @@
 public class gugudan {
     public static void main(String[] args){
-        int t_one = 2 * 1;
-        int t_two = 2 * 2;
-        int t_three = 2 * 3;
-        int t_four = 2 * 4;
-        int t_five = 2 * 5;
-        int t_six = 2 * 6;
-        int t_seven = 2 * 7;
-        int t_eight = 2 * 8;
-        int t_nine = 2 * 9;
-        int th_one = 3 * 1;
-        int th_two = 3 * 2;
-        int th_three = 3 * 3;
-        int th_four = 3 * 4;
-        int th_five = 3 * 5;
-        int th_six = 3 * 6;
-        int th_seven = 3 * 7;
-        int th_eight = 3 * 8;
-        int th_nine = 3 * 9;
+        System.out.println("2단");
+        System.out.println(2 * 1);
+        System.out.println(2 * 2);
+        System.out.println(2 * 3);
+        System.out.println(2 * 4);
+        System.out.println(2 * 5);
+        System.out.println(2 * 6);
+        System.out.println(2 * 7);
+        System.out.println(2 * 8);
+        System.out.println(2 * 9);
 
-
-        System.out.println(t_one);
-        System.out.println(t_two);
-        System.out.println(t_three);
-        System.out.println(t_four);
-        System.out.println(t_five);
-        System.out.println(t_six);
-        System.out.println(t_seven);
-        System.out.println(t_eight);
-        System.out.println(t_nine);
-        System.out.println(th_one);
-        System.out.println(th_two);
-        System.out.println(th_three);
-        System.out.println(th_four);
-        System.out.println(th_five);
-        System.out.println(th_six);
-        System.out.println(th_seven);
-        System.out.println(th_eight);
-        System.out.println(th_nine);
+        System.out.println("3단");
+        System.out.println(3 * 1);
+        System.out.println(3 * 2);
+        System.out.println(3 * 3);
+        System.out.println(3 * 4);
+        System.out.println(3 * 5);
+        System.out.println(3 * 6);
+        System.out.println(3 * 7);
+        System.out.println(3 * 8);
+        System.out.println(3 * 9);
+        
     }
     
 }
