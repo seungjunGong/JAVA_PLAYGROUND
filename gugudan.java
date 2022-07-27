487707935cb323cbf830543797fe7cf55a539b77
seungjunGong <jjw5741@naver.com>
2022-07-27 14:10:47 +0900

    4단 5단 입력 받아 출력

--- gugudan.java
+++ gugudan.java
@@ -1,64 +1,49 @@
+import java.util.Scanner;
+
 public class gugudan {
     public static void main(String[] args){
-        System.out.println("2단");
-        System.out.println(2 * 1);
-        System.out.println(2 * 2);
-        System.out.println(2 * 3);
-        System.out.println(2 * 4);
-        System.out.println(2 * 5);
-        System.out.println(2 * 6);
-        System.out.println(2 * 7);
-        System.out.println(2 * 8);
-        System.out.println(2 * 9);
-        
-        System.out.println("3단");
-        System.out.println(3 * 1);
-        System.out.println(3 * 2);
-        System.out.println(3 * 3);
-        System.out.println(3 * 4);
-        System.out.println(3 * 5);
-        System.out.println(3 * 6);
-        System.out.println(3 * 7);
-        System.out.println(3 * 8);
-        System.out.println(3 * 9);
+       System.out.println("구구단 중 출력할 단은? : ");
+       Scanner scanner = new Scanner(System.in);
+       int number = scanner.nextInt();
+       System.out.println("number : " + number);
 
-        System.out.println("4단");
-        int result = 4 * 1;
-        System.out.println(result);
-        result = 4 * 2;
-        System.out.println(result);
-        result = 4 * 3;
-        System.out.println(result);
-        result = 4 * 4;
-        System.out.println(result);
-        result = 4 * 5;
-        System.out.println(result);
-        result = 4 * 6;
-        System.out.println(result);
-        result = 4 * 7;
-        System.out.println(result);
-        result = 4 * 8;
-        System.out.println(result);
-        result = 4 * 9;
-        System.out.println(result);
+    
+       int result = number * 1;
+       System.out.println(result);
+       result = number * 2;
+       System.out.println(result);
+       result = number * 3;
+       System.out.println(result);
+       result = number * 4;
+       System.out.println(result);
+       result = number * 5;
+       System.out.println(result);
+       result = number * 6;
+       System.out.println(result);
+       result = number * 7;
+       System.out.println(result);
+       result = number * 8;
+       System.out.println(result);
+       result = number * 9;
+       System.out.println(result);
 
-        System.out.println("5단");
-        result = 5 * 1;
-        System.out.println(result);
-        result = 5 * 2;
-        System.out.println(result);
-        result = 5 * 3;
-        System.out.println(result);
-        result = 5 * 4;
-        System.out.println(result);
-        result = 5 * 5;
-        System.out.println(result);
-        result = 5 * 6;
-        System.out.println(result);
-        result = 5 * 8;
-        System.out.println(result);
-        result = 5 * 9;
-        System.out.println(result);
+       System.out.println("5단");
+       result = number * 1;
+       System.out.println(result);
+       result = number * 2;
+       System.out.println(result);
+       result = number * 3;
+       System.out.println(result);
+       result = number * 4;
+       System.out.println(result);
+       result = number * 5;
+       System.out.println(result);
+       result = number * 6;
+       System.out.println(result);
+       result = number * 8;
+       System.out.println(result);
+       result = number * 9;
+       System.out.println(result);
     }
     
 }
