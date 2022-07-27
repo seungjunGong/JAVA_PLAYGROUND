bd10157ad073effae1ee19b71c8e5fa04a39a9ea
seungjunGong <jjw5741@naver.com>
2022-07-27 11:45:59 +0900

    java playground 3times

--- javaplayground.java
+++ javaplayground.java
@@ -2,6 +2,8 @@ public class javaplayground {
     public static void main(String[] args){
         System.out.println("Hello git!");
         System.out.println("second hello");
+        System.out.println("드디어 성공!");
+
 
     }
 }
